dcc90bd23061379315acf7ef3b795cffccd482e9
bansvai <bansalvaibhav506@gmail.com>
2024-12-06 13:52:46 +0530

    Foundation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello from React!");
+const ReactComponent = () => <h1 id="heading">Hello from JSX and React</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<ReactComponent />);
